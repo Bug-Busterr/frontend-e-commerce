535f596acce32d1c65782d446eb57fe52c9d38b4
Norhanmohamed231 <norhan2312003@gmail.com>
2025-09-05 07:32:41 +0300

    Update WishlistContext.jsx

--- src/Components/WishlistContext.jsx
+++ src/Components/WishlistContext.jsx
@@ -6,21 +6,21 @@ const WishlistContext = createContext();
 export const WishlistProvider = ({ children }) => {
     const [wishlist, setWishlist] = useState([]);
 
-    // دالة لإضافة منتج إلى قائمة المفضلة
+    
     const addToWishlist = (product) => {
         setWishlist((prevWishlist) => {
-            // نتحقق أولاً هل المنتج موجود بالفعل في القائمة أم لا
+            
             const isExisting = prevWishlist.some((item) => item.id === product.id);
             if (isExisting) {
-                return prevWishlist; // إذا كان موجوداً، لا تفعل شيئاً
+                return prevWishlist; 
             } else {
-                // إذا لم يكن موجوداً، أضف المنتج الجديد إلى القائمة
+                
                 return [...prevWishlist, product];
             }
         });
     };
 
-    // دالة لحذف منتج من قائمة المفضلة
+    
     const removeFromWishlist = (id) => {
         setWishlist((prevWishlist) => prevWishlist.filter((item) => item.id !== id));
     };
@@ -31,5 +31,6 @@ export const WishlistProvider = ({ children }) => {
         </WishlistContext.Provider>
     );
 };
+
 
 export const useWishlist = () => useContext(WishlistContext);
